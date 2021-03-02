ed6ccd237b553ef56662402ae252e116d5f7b042
tbsdvl <tristonburns97@gmail.com>
2021-03-01 17:49:34 -0700

    Adding prompts for password criteria

--- script.js
+++ script.js
@@ -39,4 +39,13 @@ function generatePassword(){
   var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ';', ':', '"', ',', '.', '/', '[', ']', '|', '`', '~'];}
+  var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ';', ':', '"', ',', '.', '/', '[', ']', '|', '`', '~'];
+
+   // Provide user with different prompts to select password criteria
+   var userChoices = {
+    lower: prompt("Lowercase Characters? (Yes/No): ", "Yes"),
+    upper: prompt("Uppercase Characters? (Yes/No): ", "Yes"),
+    numbers: prompt("Numbers? (Yes/No): ", "Yes"),
+    special: prompt("Special Characters? (Yes/No): ", "Yes"),
+    passLength: prompt("Enter a number between 8 and 128 to set the length of your new password.")
+  }};
