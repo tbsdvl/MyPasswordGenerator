03b6511b066ad287e122574e83dfdb72a612843b
tbsdvl <tristonburns97@gmail.com>
2021-03-01 17:48:02 -0700

    Creating character arrays for generatePassword function

--- script.js
+++ script.js
@@ -31,4 +31,12 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Password generator function
-function generatePassword()
+function generatePassword(){
+
+  // Create arrays containing all my potential characters to generate into the new password
+  var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+  'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ';', ':', '"', ',', '.', '/', '[', ']', '|', '`', '~'];}
